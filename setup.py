b5f457cda881c70cdc48bef07798a865c150c894
Beesama <gbozee@gmail.com>
2019-11-08 04:00:21 +0100

    pin the library dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="pystripe",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",  # example license
@@ -22,7 +22,7 @@ setup(
         "Paperboy",
         "python-dateutil",
         # "requests-async",
-        "stripe",
+        "stripe==2.37.2",
         "requests-async @ https://github.com/encode/requests-async/archive/master.zip",
     ],
     extras_require={
